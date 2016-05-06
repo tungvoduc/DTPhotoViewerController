692fa2f74adc89b45c81098ea0038d54d8548412
Tung Vo <DreamBroker@Tungs-MacBook-Pro.local>
2016-05-07 02:08:40 +0300

    Initial commit

---
+++ Example/DTPhotoViewerController/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DTPhotoViewerController
+//
+//  Created by Tung Vo on 05/07/2016.
+//  Copyright (c) 2016 Tung Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
