275a4d9a1c819c71da36d44abf90901f8e62fd6e
Admin <admin@dsl-hkibrasgw5-58c04e-151.dhcp.inet.fi>
2017-08-04 16:00:55 +0300

    add vertically scrolling support

--- Example/DTPhotoViewerController/ViewController.swift
+++ Example/DTPhotoViewerController/ViewController.swift
@@ -72,7 +72,6 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
         
         if let cell = collectionView.cellForItem(at: indexPath) as? CollectionViewCell {
             if let viewController = BDFPostPhotoViewerController(referencedView: cell.imageView, image: cell.imageView.image) {
-                viewController.scrollDirection = .vertical
                 viewController.dataSource = self
                 viewController.delegate = self
                 self.present(viewController, animated: true, completion: nil)
